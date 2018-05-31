3b4c5d2df8c1d3665578451a438faf4b49f1d663
PaulaAraya <paula.arayawilliams@gmail.com>
2018-05-31 10:54:47 -0300

    Ahora se imprime el mensaje en el Div el error era de sintaxis en innerHTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 
 //crear funcion con variables window.onload de los botones, numero de desplazamiento y cajas con document.getElementByID
 //Dom de los botones
-//let boton1 = document.getElementById("boton1")
-
 window.onload = function () {
 let numberPositionDescipher = document.getElementById("numberPositionDescipher")
 let numberPositionCipher = document.getElementById("numberPositionCipher")
-
 let descipherBoxResult = document.getElementById("descipherBoxResult")
 let cipherBoxResult = document.getElementById("cipherBoxResult")
 const btnDecipher = document.getElementById("btnDecipher")
@@ -16,14 +13,17 @@ const btnCipher = document.getElementById("btnCipher")
 btnDecipher.addEventListener("click", () => {
   let descipherBoxSubmit = document.getElementById("descipherBoxSubmit").value.toUpperCase();
   console.log(descipherBoxSubmit);
-  descipherBoxResult.innerHtml = "tu resultasdo es : " + descipherBoxSubmit;
-  //descipherBoxSubmit.innerHtml = descipherBoxResult;
+  descipherBoxResult.innerHTML = descipherBoxSubmit;
 });
 
 btnCipher.addEventListener("click", () => {
   let cipherBoxSubmit = document.getElementById("cipherBoxSubmit").value.toUpperCase();
   console.log(cipherBoxSubmit);
-  cipherBoxResult.innerHtml = cipherBoxSubmit;
-});};
+  cipherBoxResult.innerHTML = cipherBoxSubmit;
+});
+
+
+
+};  /* llave de window.onload*/
 
 
